6ddfe381aaf694cdcf0536da6bd39af1737d2762
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2018-12-15 15:39:20 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Student
 attr_accessor :name, :grade
 attr_reader :id
@@ -7,21 +8,38 @@ attr_reader :id
   end
 
   def self.create_table
-    sql = << _SQL 
+    sql = <<SQL 
     CREATE TABLE IF NOT EXISTS students (
       id INTEGER PRIMARY KEY, 
       name TEXT, 
       grade TEXT
       )
-      SQL
+SQL
       DB[:conn].execute(sql)
   end
 
   def self.drop_table
-  
-  end
+    sql = <<SQL
+    DROP TABLE students;
+SQL
+    DB[:conn].execute(sql)
+
+end
 
   def save
+    sql = <<SQL
+      INSERT INTO students(name, grade) VALUES (?, ?)
+SQL
+    
+    DB[:conn].execute(sql, self.name, self.grade)
+   db_id =  DB[:conn].execute("SELECT id FROM students WHERE name = ?", self.name).flatten
+    @id = db_id[0]
+  end
+  
+  def self.create(student_hash)
+    student= self.new(student_hash[:name], student_hash[:grade])
+    student.save
+    student
   end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
